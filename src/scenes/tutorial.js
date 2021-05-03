8821af61bbb3b7887ada701d5399a2a1e639f73f
SamLuyen130 <66100661+SamLuyen130@users.noreply.github.com>
2021-05-02 19:02:33 -0700

    test

--- src/scenes/tutorial.js
+++ src/scenes/tutorial.js
@@ -9,33 +9,12 @@ class tutorial extends Phaser.Scene {
     }
 
     create() {
-
-        let tutConfig = {
-            fonFamily: 'Courier', 
-            fontSize: '24px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-            top: 5,
-            bottom: 5,
-            },
-            fixedWidth: 450
-            
-        }
       
         //background image
         this.backgroundImage = this.add.sprite(this.centerX(), this.centerY(), 'tutorialbackground');
 
         //back to menu
-<<<<<<< HEAD
         this.MenuButton = this.add.sprite(this.centerX(), this.centerY() + 250, 'MenuButton').setInteractive();
-
-        // Add Tutorial Text
-        this.add.text(game.config.width / 20 , game.config.height / 5 - borderUISize - borderPadding, 'Press ← to move left and Right', tutConfig).setOrigin(0);
-=======
-        this.MenuButton = this.add.sprite(this.centerX(), this.centerY() + 275, 'MenuButton').setInteractive();
->>>>>>> bc8c9f7ac6c1d4f7749cd089213829d78b321d13
         
         this.MenuButton.on('pointerdown', function () {
 			this.scene.start('gameMenu');
